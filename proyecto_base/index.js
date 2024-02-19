b6d45979fe8f96ec6baa1d9f6e96c22a9d3dcc00
luisNavasArg <lpipnavas@gmail.com>
2024-02-19 18:23:19 -0300

    repaso con un servidor básico

--- proyecto_base/index.js
+++ proyecto_base/index.js
@@ -1,11 +1,56 @@
-// const express = require("express");
-// const app = express();
-// app.get("/",function (req,res) {
-//     res.send("Hola curso cómo están?");
-// })
-// app.listen(8080,function () {
-//     console.log("se está ejecutando el servidor");
-// })
-const Server = require("./models/server");
-const server = new Server();
-server.listen();
+const express = require("express");
+
+const app = express();
+const cors =require("cors");
+app.use(cors());
+app.use(express.json());
+let users=[];
+let mid=0;
+//method get
+app.get("/api/users",(req,res)=>{
+
+    res.status(200).json(users);
+
+});
+//method post
+app.post("/api/users",(req,res)=>{
+    
+    let newObj =req.body;
+    mid++;
+    newObj.id=mid;
+    users=[...users,req.body];
+    res.status(201).json({"data":users});
+});
+//method put
+app.put("/api/users",(req,res)=>{
+    let id=Number(req.query.id);
+    let {name}=req.body;
+ 
+    let newData = users.map(user=>{
+        console.log(user);
+        if (id==user.id) {
+            return user={id,name};
+        }else{
+            return user;
+        }
+    
+    });
+    users=newData;
+    res.status(202).json(users);
+    
+});
+//method delete
+app.delete("/api/users",(req,res)=>{
+    let id=Number(req.query.id);
+    let newData = users.filter(user=>{ 
+        if (id!=user.id) {
+            return user;
+        }
+    });
+    users=newData;
+    res.status(202).json(users);
+});
+
+app.listen(8080,()=>{
+    console.log("Server on Ready!");
+});
--- proyecto_base/routes/users.js
+++
@@ -1,8 +0,0 @@
-const {Router} =require("express");
-const router = Router();
-
-router.get("/",function (req,res) {
-        res.send("Users petición GET");
-});
-
-module.exports=router;
